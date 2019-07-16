4ad85ce579eff6e44254a57ab1e24e3a55b52235
grammaton <grammaton@me.com>
2019-07-16 10:55:44 +0200

    seamclone

--- seamclone.sh
+++ seamclone.sh
@@ -3,11 +3,13 @@
 # use it by terminal typing:
 # bash seamclone.sh
 
-say Will do, sir
-
 cd ..
 
+git clone --recurse-submodules -v https://github.com/s-e-a-m/cs1954sks2.git
+
 git clone --recurse-submodules -v https://github.com/s-e-a-m/faust-code.git
+
+git clone --recurse-submodules -v https://github.com/s-e-a-m/faust-libraries.git
 
 git clone --recurse-submodules -v https://github.com/s-e-a-m/fc1969lais.git
 
@@ -15,12 +17,8 @@ git clone --recurse-submodules -v https://github.com/s-e-a-m/fc1987nlre.git
 
 git clone --recurse-submodules -v https://github.com/s-e-a-m/fc1991lmml.git
 
-git clone --recurse-submodules -v https://github.com/s-e-a-m/cs1954sks2.git
-
 git clone --recurse-submodules -v https://github.com/s-e-a-m/maxmsp.git
 
 git clone --recurse-submodules -v https://github.com/s-e-a-m/References.git
 
 git clone --recurse-submodules -v https://github.com/s-e-a-m/s-e-a-m.github.io.git
-
-say Congratulations! You have created a new element
