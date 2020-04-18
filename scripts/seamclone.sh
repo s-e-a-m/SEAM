c20a4bc9269a5c7953a3cfacac3a153dfc58b226
Davide Tedesco <davide_hrd@hotmail.it>
2020-04-18 17:28:24 +0200

    Update seamclone.sh

--- scripts/seamclone.sh
+++ scripts/seamclone.sh
@@ -13,6 +13,8 @@ git clone --recurse-submodules -v https://github.com/s-e-a-m/faust-libraries.git
 
 git clone --recurse-submodules -v https://github.com/s-e-a-m/fc1969lais.git
 
+git clone --recurse-submodules -v https://github.com/s-e-a-m/fc1987nlpppd.git
+
 git clone --recurse-submodules -v https://github.com/s-e-a-m/fc1987nlre.git
 
 git clone --recurse-submodules -v https://github.com/s-e-a-m/fc1991lmml.git
@@ -25,8 +27,8 @@ git clone --recurse-submodules -v https://github.com/s-e-a-m/s-e-a-m.github.io.g
 
 git clone --recurse-submodules -v https://github.com/s-e-a-m/max1969lais.git
 
-git clone --recurse-submodules -v https://github.com/s-e-a-m/seam-lac2020
+git clone --recurse-submodules -v https://github.com/s-e-a-m/seam-lac2020.git
 
-git clone --recurse-submodules -v https://github.com/s-e-a-m/seam-smc2020
+git clone --recurse-submodules -v https://github.com/s-e-a-m/seam-smc2020.git
 
-git clone --recurse-submodules -v https://github.com/s-e-a-m/snd1987nlre
+git clone --recurse-submodules -v https://github.com/s-e-a-m/snd1987nlre.git
